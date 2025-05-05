39a4f6bf21df2ccacfa1087a0b262338578cc37c
Ravi Kumar Reddy Sanikommu <ravikumarreddy.s@gmail.com>
2025-05-04 21:55:37 -0500

    First commit

---
+++ src/main/java/com/abc/rss_processor/RssProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.abc.rss_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abc/rss_processor/RssProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.rss_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
