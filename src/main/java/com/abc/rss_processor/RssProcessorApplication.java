2d2d70afd854d0cab1789fbc50494599c272e814
Ravi Kumar Reddy Sanikommu <ravikumarreddy.s@gmail.com>
2025-05-07 08:31:44 -0500

    Added docker container capability

--- src/main/java/com/abc/rss_processor/RssProcessorApplication.java
+++ src/main/java/com/abc/rss_processor/RssProcessorApplication.java
@@ -2,8 +2,10 @@ package com.abc.rss_processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RssProcessorApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/abc/rss_processor/scheduler/RssMessageScheduler.java
@@ -0,0 +1,30 @@
+package com.abc.rss_processor.scheduler;
+
+import com.abc.rss_processor.model.RssFeed;
+import com.abc.rss_processor.service.RssMessagePublisher;
+import com.abc.rss_processor.service.RssService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class RssMessageScheduler {
+
+    @Autowired
+    private RssService rssService;
+
+    @Autowired
+    private RssMessagePublisher rssMessagePublisher;
+
+    @Value("${rss.feed.url}")
+    private String rssFeedURL;
+
+    @Scheduled(cron = "0 */1 * ? * *")
+    public void pollNewsFeed() throws Exception {
+        RssFeed rssFeed=rssService.getFeed(rssFeedURL);
+        rssMessagePublisher.sendMessageToTopic(rssFeed);
+    }
+
+}
